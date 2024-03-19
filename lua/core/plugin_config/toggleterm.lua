6e208ad32378fe7e567d4b1b93fd3b2d1e800597
Nikita Skhabuitskyi <nikitaskh211@gmail.com>
2024-03-19 14:09:28 +0200

    Configured ToggleTerm to use PowerShell

--- lua/core/plugin_config/toggleterm.lua
+++ lua/core/plugin_config/toggleterm.lua
@@ -2,6 +2,7 @@ require('toggleterm').setup{
   size = 20,
   open_mapping = [[<c-t>]],
   hide_numbers = true, -- Hide the window number for the terminal windows
+  shell = 'pwsh.exe',
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = 0.3, -- 0 to 1 for the amount of shading to apply
